24615f8367a6039b5ec0a02a09ded8d2930a2f67
Michael Walker <michaelwalker@quickenloans.com>
2021-09-27 13:24:19 -0400

    Wrote code to read an example file in rust
    - added the example file
    - changed cargo name to use underscores (matching the approved naming convention of rust)
    - added a structured storage library for building the structured storage code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+use std::io::prelude::*;
+use std::fs::File;
+
+fn main() -> io::Result<()> {
+    let mut f = File::open("file-sample_100kB.doc")?;
+    let mut buffer = Vec::new();
+
+    // reads all the bytes
+    let n = f.read_to_end(&mut buffer)?;
+
+    println!("length of the buffer: {:?}", n);
+    Ok(())
 }
